a724a858554d374a670a26bb71de304b2d82c39e
FLYiNg_hbt <flying_hbt@163.com>
2015-09-02 18:07:20 +0800

    Android 中 Action Bar 的使用--1.2.3.4 2. 启用 Action Bar 3. 在 Action Bar 上添加按钮 4. 自定义 Action Bar 样式

--- app/src/main/java/com/example/mypegasus/usingactionbar/SecondActivity.java
+++ app/src/main/java/com/example/mypegasus/usingactionbar/SecondActivity.java
@@ -1,24 +1,29 @@
 package com.example.mypegasus.usingactionbar;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by MyPegasus on 2015/9/2.
+ */
+public class SecondActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		setContentView(R.layout.activity_second);
+		// 貌似AndroidManifest.xml 中配置过parentActivity相关属性，就会自动显示出左上角的up button
+//		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
+		getMenuInflater().inflate(R.menu.menu_second, menu);
 		return true;
 	}
 
@@ -35,18 +40,12 @@ public class MainActivity extends AppCompatActivity {
 		}*/
 
 		switch (id) {
-			case R.id.home:
-				break;
-			case android.R.id.home:// 这个才是左上角图标id
-				finish();
-				break;
 			case R.id.action_search:
 				//Toast.makeText(this, "Action_Search", Toast.LENGTH_SHORT).show();
-				startActivity(new Intent(this, SecondActivity.class));
+				startActivity(new Intent(this, ThirdActivity.class));
 				break;
 			case R.id.action_setting:
-				//Toast.makeText(this, "Action_Setting", Toast.LENGTH_SHORT).show();
-				startActivity(new Intent(this, ThirdActivity.class));
+				Toast.makeText(this, "Action_Setting", Toast.LENGTH_SHORT).show();
 				break;
 		}
 
